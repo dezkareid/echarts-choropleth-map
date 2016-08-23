63073c14949d5cca43e4801ef42c7e5a3685c1b7
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-08-23 12:44:11 -0500

    Se agrego algoritmo para cambiar el color del texto dependiendo del fondo

--- treemap.js
+++ treemap.js
@@ -1,3 +1,22 @@
+function hexToRGB (hex) {
+	var r,g,b;
+	hex = hex.replace('#','');
+  r = parseInt(hex.substring(0,2), 16);
+  g = parseInt(hex.substring(2,4), 16);
+  b = parseInt(hex.substring(4,6), 16);
+	return [r,g,b];
+}
+function getBestTextColor (rgb) {
+	 var o = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parseInt(rgb[2]) * 114)) /1000);
+    
+    console.log(o);
+    
+    if(o > 125) {
+      return "#000000";
+    }else{ 
+      return "#FFFFFF";
+    }
+}
 function initTreemap (items, property_text, property_value, colors) {
 	var d3 = Plotly.d3;
 	var max_value = d3.max( items, function(item) { return item[property_value] });
@@ -18,32 +37,35 @@ function initTreemap (items, property_text, property_value, colors) {
 	var rectangles = Treemap.generate(values, 100, 100);
 
 	for (var i in rectangles) {
+		var color = colorScale([values[counter]]);
 		var shape = {
-		        		type: 'rect',
-						x0: rectangles[i][0],
-						y0: rectangles[i][1],
-						x1: rectangles[i][2],
-						y1: rectangles[i][3],
-						line: {
-								width: 2
-							},
-						fillcolor: colorScale([values[counter]])
-				};
+		  type: 'rect',
+			x0: rectangles[i][0],
+			y0: rectangles[i][1],
+			x1: rectangles[i][2],
+			y1: rectangles[i][3],
+			line: {
+				width: 2
+			},
+			fillcolor: color
+		};
+
 		shapes.push(shape);
 		var annotation = {
-		    				x: (rectangles[i][0] + rectangles[i][2]) / 2,
-							y: (rectangles[i][1] + rectangles[i][3]) / 2,
-							text: String(values[counter]),
-							showarrow: false
-				};
+		  x: (rectangles[i][0] + rectangles[i][2]) / 2,
+			y: (rectangles[i][1] + rectangles[i][3]) / 2,
+			text: String(values[counter]),
+			font: {
+        color: getBestTextColor(hexToRGB(color))
+      },
+			showarrow: false
+		};
 		annotations.push(annotation);
 		
-		// For Hover Text
 		x_trace.push((rectangles[i][0] + rectangles[i][2]) / 2);
 		y_trace.push((rectangles[i][1] + rectangles[i][3]) / 2);
 		text.push(String(items[counter][property_text]));
-				
-		// Incrementing Counter		
+		
 		counter++;
 	}
 
